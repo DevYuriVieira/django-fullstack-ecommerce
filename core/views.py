06ff67a146b75f90b7cf261dd61ca3834e9d4b08
Yuri <yurivieirateixeira@hotmail.com>
2026-03-22 00:09:44 -0300

    feat: implement product search functionality in views

--- core/views.py
+++ core/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from .models import Produto
 
-# Create your views here.
 def home(request):
     produtos = Produto.objects.all()
+    
+    termo_buscado = request.GET.get('q')
+
+    if termo_buscado:
+        produtos = produtos.filter(title__icontains=termo_buscado)
 
     meu_dicionario_python = {
-        'lista_produtos': produtos
+        'lista_produtos': produtos,
+        'termo_buscado': termo_buscado 
     }
 
     return render(request, 'home.html', context=meu_dicionario_python)
