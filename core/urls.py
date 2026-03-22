232e9be3dacb634a8ac57895f20db064568c37f5
Yuri <yurivieirateixeira@hotmail.com>
2026-03-22 19:33:32 -0300

    feat: setup Django auth views, core routing, and dynamic navbar logic

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('minha-conta/', views.minha_conta, name='minha_conta'),
+    
+    path('login/', views.login_view, name='login'),
+    path('cadastro/', views.cadastro_view, name='cadastro'),
+    path('logout/', views.logout_view, name='logout'),
+]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required 
 from .models import Produto
 
 def home(request):
@@ -16,5 +19,34 @@ def home(request):
 
     return render(request, 'home.html', context=meu_dicionario_python)
 
+@login_required(login_url='/login/')
 def minha_conta(request):
     return render(request, 'minha_conta.html')
+
+def cadastro_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user) 
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    
+    return render(request, 'cadastro.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('minha_conta') 
+    else:
+        form = AuthenticationForm()
+        
+    return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
