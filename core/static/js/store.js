c03525434812587687fcc5727267ab6a46a8a532
Yuri <yurivieirateixeira@hotmail.com>
2026-03-23 13:56:48 -0300

    feat: implement vanilla JS state management store for frontend data

---
+++ core/static/js/store.js
@@ -0,0 +1,29 @@
+const Store = {
+    state: {
+        carrinho: JSON.parse(localStorage.getItem('meuCarrinho')) || [],
+        descontoAtivo: parseFloat(localStorage.getItem('meuCupom')) || 0
+    },
+    
+    listeners: [],
+
+    subscribe(fn) {
+        this.listeners.push(fn);
+    },
+
+    notify() {
+        this.listeners.forEach(fn => fn(this.state));
+    },
+
+    setState(newState) {
+        this.state = { ...this.state, ...newState };
+
+        if (newState.carrinho !== undefined) {
+            localStorage.setItem('meuCarrinho', JSON.stringify(this.state.carrinho));
+        }
+        if (newState.descontoAtivo !== undefined) {
+            localStorage.setItem('meuCupom', this.state.descontoAtivo.toString());
+        }
+
+        this.notify();
+    }
+};
